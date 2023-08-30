bd6b8a561319b149e6f4b20f479ae7c665e5b51b
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-30 21:14:12 +0600

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+const x: number = 10;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+} else {
+    console.log("x is not greater than 5");
+}
+
+switch (x) {
+    case 1:
+        console.log("x is 1");
+        break;
+    case 2:
+        console.log("x is 2");
+        break;
+    default:
+        console.log("x is not 1 or 2");
+}
